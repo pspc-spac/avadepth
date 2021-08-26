2137a48823bc0a5eb55603060bff3157ddd2d5cd
jiw <William.Ji@tpsgc-pwgsc.gc.ca>
2021-08-26 14:04:05 -0700

    AVD-127 Formatted the code

--- scripts/incl_ava_defs-fra.js
+++ scripts/incl_ava_defs-fra.js
@@ -174,24 +174,50 @@ var padZero = function(num){
               {tag: "label", attr: {for: "layers_ttl", style: "font-weight:bold"}, child: ["Couches:"]},
               {tag: "div", child: [
                   {tag: "input", attr: { type: "checkbox", id: "chkLyrSounding", value: "soundings", checked: "checked" } },
-                  {tag: "div", attr: { class: "popup" }, child: [" sondages", { tag: "span", attr: { class: "popuptext" }, child: [" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."] }] },
+                  {tag: "div", attr: { class: "popup" }, child: [
+                          " sondages", {tag: "span", attr: { class: "popuptext" }, child: [
+                                  " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."]
+                          }]
+                  },
                   {tag: "br"},
                   {tag: "input", attr: { type: "checkbox", id: "chkLyrChannel", value: "channel", checked: "checked" } },
-                  {tag: "div", attr: { class: "popup" }, child: [" chenal", { tag: "span", attr: { class: "popuptext" }, child: [" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."] }] },
+                  {tag: "div", attr: { class: "popup" }, child: [
+                          " chenal", {tag: "span", attr: { class: "popuptext" }, child: [
+                                  " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."]
+                          }]
+                  },
                   {tag: "br"},
                   {tag: "input", attr: { type: "checkbox", id: "chkLyrSurface", value: "surface", checked: "checked" } },
-                  {tag: "div", attr: { class: "popup" }, child: [" surfaces:", { tag: "span", attr: { class: "popuptext" }, child: [" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."] }] },
+                  {tag: "div", attr: { class: "popup" }, child: [
+                          " surfaces:", {tag: "span", attr: { class: "popuptext" }, child: [
+                                  " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."]
+                          }]
+                  },
                   {tag: "br"},
-                  {tag: "div", attr: {class: "optionIndent"}, child: [
-                      {tag: "label", attr: {for: "surfTrans"}, child: ["transparence"]},
+                  {tag: "div", attr: { class: "optionIndent" }, child: [
+                          {tag: "label", attr: { for: "surfTrans" }, child: [
+                            {tag: "div", attr: { class: "popup" }, child: [
+                                  "transparence", {tag: "span", attr: { class: "popuptext" }, child: [
+                                      " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."]
+                                  }]
+                              }]
+                          },
                       {tag: "input", attr: {id: "surfTrans", type: "range", min: "0", max: "100", value: "100"}},
                       "type de surface:",
                       {tag: "div", attr: {class: "optionIndent"}, child: [
                           {tag: "input", attr: { type: "radio", name: "surface", value: "combined", checked: "checked" } },
-                          {tag: "div", attr: { class: "popup" }, child: [" bathymetrique", { tag: "span", attr: { class: "popuptext" }, child: [" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."] }] },
+                          {tag: "div", attr: { class: "popup" }, child: [
+                                  " bathymetrique", {tag: "span", attr: { class: "popuptext" }, child: [
+                                          " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."]
+                              }]
+                          },
                           {tag: "br"},
                           {tag: "input", attr: { type: "radio", name: "surface", value: "conformance" } },
-                          {tag: "div", attr: { class: "popup" }, child: [" conformité", { tag: "span", attr: { class: "popuptext" }, child: [" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."] }] },
+                          {tag: "div", attr: { class: "popup" }, child: [
+                                  " conformité", {tag: "span", attr: { class: "popuptext" }, child: [
+                                          " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."]
+                                  }]
+                          },
                           {tag: "br"}
                       ]},
                       "détails:",
